4daa5a2a0141ec85ae564ea233f13bb727272656
SaffronLilac <bella.gereeva@yandex.ru>
2023-10-26 22:36:17 +0300

    Update server URL

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://eba72b17-1bd8-4765-aaf4-4ee7dcf8ac71.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://571ad9ad-a568-4571-adeb-bba453cb3b15.serverhub.praktikum-services.ru"
 CREATE_ORDER_PATH = "/api/v1/orders"
 GET_ORDER_PATH = "/api/v1/orders/track"
